ad3b655dc28b08f04f27fb460bb13bc8b9d1778d
cheezgi <zackh@firemail.cc>
2017-05-16 18:32:27 -0400

    move numbers/symbols

--- keymap.c
+++ keymap.c
@@ -69,7 +69,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     // lower: symbols, arrows, mouse movement
     [__LOWER] = {
-      {KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_MINS, KC_EQL,  _______},
+      {KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______},
       {_______, KC_PSCR, KC_INS,  KC_HOME, KC_END,  _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, KC_BSLS},
       {_______, _______, _______, _______, _______, _______, _______, _______, KC_BTN1, KC_MS_U, KC_BTN2, KC_WH_U},
       {_______, _______, _______, _______, _______, _______, _______, _______, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_D}
@@ -77,10 +77,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     // raise: ()[]{}, numbers, misc movement
     [__RAISE] = {
-      {_______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______},
-      {_______, _______, KC_LCBR, KC_LBRC, KC_LPRN, _______, _______, KC_RPRN, KC_RBRC, KC_RCBR, _______, _______},
-      {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______},
-      {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______}
+      {S(KC_GRV),KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_MINS, KC_EQL,  _______},
+      {_______,  _______, KC_LCBR, KC_LBRC, KC_LPRN, _______, _______, KC_RPRN, KC_RBRC, KC_RCBR, _______, _______},
+      {_______,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______},
+      {_______,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______}
     },
 
     // roll: macros for sharex screen capture, virtualbox management, audio
